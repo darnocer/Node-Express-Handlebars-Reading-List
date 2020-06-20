98239396cd825aa2048774217dc7a6653eadad2a
darnocer <darnocer@gmail.com>
2020-06-20 15:28:23 -0600

    seeds and schema

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+USE burgers_db;
+
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("Double Bacon Cheese Burger", false),
+    ("BBQ & Jalepeno Burger", true),
+    ("Mushroom & Swiss Burger", false),
+    ("Veggie Burger", false);
