e29fb219ca6cc45da38b1db01669f3767579a504
darnocer <darnocer@gmail.com>
2020-06-20 12:47:21 -0600

    express server setup and mysql connection

---
+++ server.js
@@ -0,0 +1,36 @@
+// DEPENDENCIES
+const express = require("express");
+
+// express app
+var app = express();
+
+// Set the port
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "burgers_db",
+});
+
+connection.connect(function (err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
+
+  console.log("connected as id " + connection.threadId);
+});
+
+app.listen(PORT, function () {
+  console.log("Server listening on: http://localhost:" + PORT);
+});
