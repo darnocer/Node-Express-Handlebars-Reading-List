4133370c3840d8e063bd2873bd6bbecbb598afc3
darnocer <darnocer@gmail.com>
2020-06-20 18:13:07 -0600

    burg controller

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,5 +1,18 @@
 const express = require("express");
+const router = express.Router();
 
 const burger = require("../models/buger.js");
+
+router.get("/", function (req, res) {
+  console.log("GET REQUEST");
+});
+
+router.post("/api/burgers", function (req, res) {
+  console.log("POST REQUEST");
+});
+
+router.put("/api/burgers/:id", function (req, res) {
+  console.log("PUT REQUEST");
+});
 
 module.exports = router;
