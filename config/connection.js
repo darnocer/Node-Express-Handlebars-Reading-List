0ebf295dc5b727bd02f34a2698c5f90880019ff1
darnocer <darnocer@gmail.com>
2020-06-25 18:22:39 -0600

    repo name in package

--- config/connection.js
+++ config/connection.js
@@ -1,19 +1,19 @@
 // DEPENDENCIES
 const mysql = require("mysql");
-const connection;
+var connection;
 
 // mysql connection
 
 if (process.env.JAWSDB_URL) {
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "books_db",
-});
+  connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "books_db",
+  });
 }
 
 // make connection
